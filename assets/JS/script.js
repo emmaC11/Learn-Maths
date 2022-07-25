e839853ad65c56506b021cc50fdd286325fd0f57
EmmaC <c.emma2112@gmail.com>
2022-07-25 10:52:12 +0000

    add code to generate two random numbers for the game question

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -21,7 +21,14 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 })
 
+/**
+ * The main game 'loop', called when the script is loaded
+ * and after the users answer has been processed
+ */
+
 function runGame() {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
 }
 
